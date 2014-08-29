3e5a89cc0bccc7195ee18d6fcae6e1a702aaf431
Rui Peres <rui.peres@williamhill.com>
2014-08-29 16:48:48 +0100

    Added a Controller, to abstract the interaction between the requests and the databse

---
+++ db/controller.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"os"
+    "net/http"
+ 	"fmt"
+
+	"gopkg.in/mgo.v2"
+    "github.com/zenazn/goji/web"
+)
+
+const (
+	mongoURL = "MONGOHQ_URL"
+)
+
+type Controller struct {
+    session *mgo.Session
+}
+
+func NewController() (*Controller, error) {
+
+    uri := os.Getenv(mongoURL)
+    if uri == "" {
+        return nil, fmt.Errorf("no DB connection string provided")
+    }
+
+    session, err := mgo.Dial(uri)
+    if err != nil {
+        return nil, err
+    }
+    session.SetMode(mgo.Monotonic, true)
+
+    return &Controller{
+        session: session,
+    }, nil
+}
+
+func (controller *Controller) CloseSession() {
+    controller.session.Close()
+}
+
+func (controller *Controller) PostUser(c web.C, w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (controller *Controller) GetUser(c web.C, w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (controller *Controller) DeleteUser(c web.C, w http.ResponseWriter, r *http.Request) {
+
+}
--- octify.go
+++ octify.go
@@ -1,58 +1,31 @@
 package main
 
 import (
-	"os"
-	"net/http"
+	"log"
 
-	"github.com/ruiaaperes/octify/utilities"
+	"github.com/ruiaaperes/octify/db"
 
 	"github.com/zenazn/goji"
-	"github.com/zenazn/goji/web"
-	"gopkg.in/mgo.v2"
 )
 
-var (
-	session *mgo.Session
+// HTTP Handlers
+
+const (
+	userURLPath = "/user"
 )
 
 func main() {
 
-	session = setupDB()
-	defer session.Close()
+	controller, error := db.NewController()
+	if error != nil {
+		  log.Fatal(error)
+	}
 
-	goji.Post("/user", postUser)
-	goji.Get("/user", getUser)
-	goji.Delete("/user", deleteUser)
+    defer controller.CloseSession()
+
+	goji.Post(userURLPath, controller.PostUser)
+	goji.Get(userURLPath, controller.GetUser)
+	goji.Delete(userURLPath, controller.DeleteUser)
 
 	goji.Serve()
 }
-
-// DB
-
-func setupDB() *mgo.Session {
-	uri := os.Getenv("MONGOHQ_URL")
-	if uri == "" {
-		panic("no DB connection string provided")
-	}
-
-	session, err := mgo.Dial(uri)
-	utilities.PanicError(err)
-	session.SetMode(mgo.Monotonic, true)
-
-	return session
-}
-
-// HTTP Handlers
-
-func postUser(c web.C, w http.ResponseWriter, r *http.Request) {
-
-}
-
-func getUser(c web.C, w http.ResponseWriter, r *http.Request) {
-
-}
-
-func deleteUser(c web.C, w http.ResponseWriter, r *http.Request) {
-
-}
-
